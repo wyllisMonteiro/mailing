3b2a92abb5b332eec7892f1fce28622b34d81e3d
Wyllis Monteiro <wyllismonteiro@gmail.com>
2020-06-23 12:20:46 +0200

    feat: check if login and pass match together in bdd

--- client/go.mod
+++ client/go.mod
@@ -2,4 +2,7 @@ module example.com/m
 
 go 1.14
 
-require github.com/dgrijalva/jwt-go v3.2.0+incompatible // indirect
+require (
+	github.com/dgrijalva/jwt-go v3.2.0+incompatible // indirect
+	github.com/go-sql-driver/mysql v1.5.0 // indirect
+)
--- client/main.go
+++ client/main.go
@@ -6,11 +6,20 @@ import (
 	"time"
 	"net/http"
 	//"io/ioutil"
+	"strconv"
+	"database/sql"
+    _ "github.com/go-sql-driver/mysql"
 	jwt "github.com/dgrijalva/jwt-go"
 )
 
 var mySigningKey = []byte("mysupersecret")
 var serverUrl = "http://localhost:9000/"
+
+type User struct {
+    ID   int    `json:"id"`
+    Login string `json:"login"`
+    Password string `json:"password"`
+}
 
 func homePage(w http.ResponseWriter, r *http.Request) {
 	validToken, err := GenerateJWT()
@@ -61,8 +70,53 @@ func handleRequests() {
 	log.Fatal(http.ListenAndServe(":9001", nil))
 }
 
+func connectToBDD() (*sql.DB, error){
+	db, err := sql.Open("mysql", "root:root@tcp(127.0.0.1:3306)/mailing")
+
+    if err != nil {
+		return nil, err
+	} else {
+		fmt.Println("Logged to database")
+		return db, nil
+	}
+}
+
+func getOneUser(login string, password string) (User, error){
+
+	db, err := connectToBDD()
+	
+	defer db.Close()
+
+	var user User
+
+	if err != nil {
+		user = User {}
+		return user, err
+	}
+
+	err = db.QueryRow("SELECT * FROM user WHERE login = ? AND password = ?", login, password).Scan(&user.ID, &user.Login, &user.Password)
+	
+	if err != nil {
+		user = User {}
+		return user, err
+	}
+
+	return user, nil 
+}
+
 func main() {
 	fmt.Println("My Simple Client")
 
+	user, err := getOneUser("wyllis", "w")
+
+	if err != nil {
+		panic(err.Error())
+		return
+	}
+
+	log.Printf(strconv.Itoa(user.ID))
+	log.Printf(user.Login)
+	log.Printf(user.Password)
+	
 	handleRequests()
 }
