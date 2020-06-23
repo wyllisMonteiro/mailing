3b832aeb43007de81bd5a25ffdbad3e63f12de01
Wyllis Monteiro <wyllismonteiro@gmail.com>
2020-06-23 15:47:29 +0200

    feat: add argon alogrithm for password hash

--- client/go.mod
+++ client/go.mod
@@ -5,4 +5,5 @@ go 1.14
 require (
 	github.com/dgrijalva/jwt-go v3.2.0+incompatible // indirect
 	github.com/go-sql-driver/mysql v1.5.0 // indirect
+	golang.org/x/crypto v0.0.0-20200622213623-75b288015ac9 // indirect
 )
--- client/main.go
+++ client/main.go
@@ -5,11 +5,17 @@ import (
 	"fmt"
 	"time"
 	"net/http"
+	"errors"
+	"crypto/subtle"
+	"strings"
 	//"io/ioutil"
 	"strconv"
+	"crypto/rand"
+	"encoding/base64"
 	"database/sql"
     _ "github.com/go-sql-driver/mysql"
 	jwt "github.com/dgrijalva/jwt-go"
+	"golang.org/x/crypto/argon2"
 )
 
 // DB CONST
@@ -21,6 +27,19 @@ var TABLE_DB 		= "mailing"
 // JWT CONST
 var MY_SIGNING_KEY 	= []byte("mysupersecret")
 var SERVER_URL 		= "http://localhost:9000/"
+
+var (
+    ErrInvalidHash         = errors.New("the encoded hash is not in the correct format")
+    ErrIncompatibleVersion = errors.New("incompatible version of argon2")
+)
+
+type params struct {
+    memory      uint32
+    iterations  uint32
+    parallelism uint8
+    saltLength  uint32
+    keyLength   uint32
+}
 
 type User struct {
     ID   int    `json:"id"`
@@ -87,7 +106,7 @@ func connectToBDD() (*sql.DB, error){
 	}
 }
 
-func getOneUser(login string, password string) (User, error){
+func getOneUser(login string) (User, error){
 
 	db, err := connectToBDD()
 	
@@ -100,7 +119,7 @@ func getOneUser(login string, password string) (User, error){
 		return user, err
 	}
 
-	err = db.QueryRow("SELECT * FROM user WHERE login = ? AND password = ?", login, password).Scan(&user.ID, &user.Login, &user.Password)
+	err = db.QueryRow("SELECT * FROM user WHERE login = ?", login).Scan(&user.ID, &user.Login, &user.Password)
 	
 	if err != nil {
 		user = User {}
@@ -110,10 +129,94 @@ func getOneUser(login string, password string) (User, error){
 	return user, nil 
 }
 
+func generateFromPassword(password string, p *params) (encodedHash string, err error) {
+    salt, err := generateRandomBytes(p.saltLength)
+    if err != nil {
+        return "", err
+    }
+
+    hash := argon2.IDKey([]byte(password), salt, p.iterations, p.memory, p.parallelism, p.keyLength)
+
+    // Base64 encode the salt and hashed password.
+    b64Salt := base64.RawStdEncoding.EncodeToString(salt)
+    b64Hash := base64.RawStdEncoding.EncodeToString(hash)
+
+    // Return a string using the standard encoded hash representation.
+    encodedHash = fmt.Sprintf("$argon2id$v=%d$m=%d,t=%d,p=%d$%s$%s", argon2.Version, p.memory, p.iterations, p.parallelism, b64Salt, b64Hash)
+
+    return encodedHash, nil
+}
+
+func generateRandomBytes(n uint32) ([]byte, error) {
+    b := make([]byte, n)
+    _, err := rand.Read(b)
+    if err != nil {
+        return nil, err
+    }
+
+    return b, nil
+}
+
+func comparePasswordAndHash(password, encodedHash string) (match bool, err error) {
+    // Extract the parameters, salt and derived key from the encoded password
+    // hash.
+    p, salt, hash, err := decodeHash(encodedHash)
+    if err != nil {
+        return false, err
+    }
+
+    // Derive the key from the other password using the same parameters.
+    otherHash := argon2.IDKey([]byte(password), salt, p.iterations, p.memory, p.parallelism, p.keyLength)
+
+    // Check that the contents of the hashed passwords are identical. Note
+    // that we are using the subtle.ConstantTimeCompare() function for this
+    // to help prevent timing attacks.
+    if subtle.ConstantTimeCompare(hash, otherHash) == 1 {
+        return true, nil
+    }
+    return false, nil
+}
+
+func decodeHash(encodedHash string) (p *params, salt, hash []byte, err error) {
+    vals := strings.Split(encodedHash, "$")
+    if len(vals) != 6 {
+        return nil, nil, nil, ErrInvalidHash
+    }
+
+    var version int
+    _, err = fmt.Sscanf(vals[2], "v=%d", &version)
+    if err != nil {
+        return nil, nil, nil, err
+    }
+    if version != argon2.Version {
+        return nil, nil, nil, ErrIncompatibleVersion
+    }
+
+    p = &params{}
+    _, err = fmt.Sscanf(vals[3], "m=%d,t=%d,p=%d", &p.memory, &p.iterations, &p.parallelism)
+    if err != nil {
+        return nil, nil, nil, err
+    }
+
+    salt, err = base64.RawStdEncoding.DecodeString(vals[4])
+    if err != nil {
+        return nil, nil, nil, err
+    }
+    p.saltLength = uint32(len(salt))
+
+    hash, err = base64.RawStdEncoding.DecodeString(vals[5])
+    if err != nil {
+        return nil, nil, nil, err
+    }
+    p.keyLength = uint32(len(hash))
+
+    return p, salt, hash, nil
+}
+
 func main() {
 	fmt.Println("My Simple Client")
 
-	user, err := getOneUser("wyllis", "w")
+	user, err := getOneUser("wyllis")
 
 	if err != nil {
 		panic(err.Error())
@@ -123,6 +226,30 @@ func main() {
 	log.Printf(strconv.Itoa(user.ID))
 	log.Printf(user.Login)
 	log.Printf(user.Password)
+
+	/*p := &params{
+        memory:      64 * 1024,
+        iterations:  3,
+        parallelism: 2,
+        saltLength:  16,
+        keyLength:   32,
+    }
+
+    // Pass the plaintext password and parameters to our generateFromPassword
+    // helper function.
+    encodedHash, err := generateFromPassword("w", p)
+    if err != nil {
+        log.Fatal(err)
+	}*/
+
+
+	
+	match, err := comparePasswordAndHash("w", user.Password)
+    if err != nil {
+        log.Fatal(err)
+    }
+
+	fmt.Printf("Match: %v\n", match)
 
 	handleRequests()
 }
